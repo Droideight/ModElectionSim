c642b868ababbb78c2336438cd458585399234c7
PIN <vincentking700@gmail.com>
2022-04-22 02:30:22 +0800

    加入專案檔案。

---
+++ ModElectionSim/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ModElectionSim
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static string setting = "nothing";
+        string EnteredValueCs = "nothing";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EditCAN1Name_Click(object sender, RoutedEventArgs e)
+        {
+            setting = "Name1";
+        }
+
+        private void EditCAN2Name_Click(object sender, RoutedEventArgs e)
+        {
+            setting = "Name2";
+        }
+    
+        private void EditPVI_Click(object sender, RoutedEventArgs e)
+        {
+            setting = "PVI";
+        }
+        
+
+        private void Edit_Enthusiasm_Click(object sender, RoutedEventArgs e)
+        {
+            setting = "Enthusiasm";
+        }
+
+
+        private void Edit_Random_Click(object sender, RoutedEventArgs e)
+        {
+            setting = "Random";
+        }
+
+        private void ConfirmRuntime_Click(object sender, RoutedEventArgs e)
+        {
+            EnteredValueCs = (string)Runtime_Enter.Text;
+            SetupManager.enteredvaluecs = EnteredValueCs.ToString();
+            SetupManager.ChangeVariable("Runtime");
+        }
+
+        private void WildCardSet_Click(object sender, RoutedEventArgs e)
+        {
+            EnteredValueCs = (string)WildCardEnterBox.Text;
+            switch (setting) {
+                case "Name1":
+                    Can1Name.Content = EnteredValueCs;
+                    SetupManager.enteredvaluecs = EnteredValueCs.ToString();
+                    SetupManager.ChangeVariable("Name1");
+                    break;
+                case "Name2":
+                    Can2Name.Content = EnteredValueCs;
+                    SetupManager.enteredvaluecs = EnteredValueCs.ToString();
+                    SetupManager.ChangeVariable("Name2");
+                    break;
+                case "PVI":
+                    SetupManager.enteredvaluecs = EnteredValueCs.ToString();
+                    SetupManager.ChangeVariable("PVI");
+                    break;
+                case "Enthusiasm":
+                    SetupManager.enteredvaluecs = EnteredValueCs.ToString();
+                    SetupManager.ChangeVariable("Enthusiasm");
+                    break;
+                case "Random":
+                    SetupManager.enteredvaluecs = EnteredValueCs.ToString();
+                    SetupManager.ChangeVariable("Random");
+                    break;
+                default:
+                    break;
+            }
+        }
+      
+    }
+}
---
+++ ModElectionSim/SetupManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModElectionSim
+{
+    internal class SetupManager
+    {
+        public static void ChangeVariable(string type)
+        {
+            switch (type)
+            {
+                case "Name1":
+                    Candidate1Name = enteredvaluecs;
+                    break;
+                case "Name2":
+                    Candidate2Name = enteredvaluecs;
+                    break;
+                case "PVI":
+                    PVI = enteredvaluecs;
+                    break;
+                case "Enthusiasm":
+                    Enthusiasm = enteredvaluecs;
+                    break;
+                case "Random":
+                    Random = enteredvaluecs;
+                    break;
+                case "Runtime":
+                    runtime = enteredvaluecs;
+                    break;
+                default:
+                    break;
+            }
+            return;
+        }
+        private static string EnteredValueCs;
+        private static string Can1Name = "Macron";
+        private static string Can2Name = "Le Pen";
+        public static string enteredvaluecs { get { return EnteredValueCs; } set { EnteredValueCs = value; } }
+        public static string Candidate1Name {
+            get { return Can1Name; } set { Can1Name = EnteredValueCs; } 
+        }
+        public static string Candidate2Name
+        {
+            get { return Can2Name; } set { Can2Name = EnteredValueCs; } 
+        }
+        private static string pvi = "0";
+        private static string random = "0";
+        private static string enthusiasm = "0";
+        public static string PVI { get { return pvi; } set { pvi = EnteredValueCs; } }
+        public static string Random { get { return random; } set { random = EnteredValueCs; } }
+        public static string Enthusiasm { get { return enthusiasm; } set { enthusiasm = EnteredValueCs; } }
+        private static string runtime = "0";
+        public static string RUNTIME { get { return runtime; } set { runtime = EnteredValueCs; } }
+    }
+}
